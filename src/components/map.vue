dc7f24a89026fc80bdcb1952ff50d6b29dc40a76
Caro-L-dev <liardcar@gmail.com>
2021-07-29 15:41:40 +0200

    :hammer: Test to displays markers on map

--- src/components/map.vue
+++ src/components/map.vue
@@ -16,6 +16,12 @@
           :url="url"
    >
         </l-tile-layer>
+        <l-marker
+          v-form="marker in markers"
+          :key="marker.id"
+          :lat-lng="marker.coordinates"
+          >
+        </l-marker>
       </l-map>
     </div>
 
@@ -23,19 +29,27 @@
 </template>
 
 <script>
-  import { LMap, LTileLayer } from 'vue2-leaflet';
+  import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export default {
  components: {
    LMap,
-   LTileLayer
+   LTileLayer,
+   LMarker
  },
  data () {
    return {
      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
      center: [ 49.1193089, 6.1757156 ],
      zoom: 12,
+       markers: [
+          {id: 1, coordinates: [ 49.114910, 6.178810 ]},
+          {id: 2, coordinates: [ 49.133290, 6.154370 ]},
+          {id: 3, coordinates: [ 49.102160, 6.158850 ]},
+          {id: 4, coordinates: [ 49.136010, 6.199630 ]},
+          {id: 5, coordinates: [ 49.105563, 6.182234 ]},
+        ]
    }
  },
  methods: {
