962e5737d7704e9c384753f675a90f1f726afed6
Caro-L-dev <liardcar@gmail.com>
2021-07-30 10:53:49 +0200

    :sparkles: Create search facility

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,45 @@
 <template>
  <div id="app">
-   <!--<HereMap :center="center"/> -->
+   <input type="text" v-model="search" >
+   <div v-for="adress in filteredAdresses" :key="adress.id">
+     <adress :adress="adress"></adress>
+   </div>
    <Map/>
   </div>
 </template>
 
 <script>
 import Map from './components/map.vue';
+import Adress from './components/Adress.vue';
 
 export default {
  name: 'App',
  components: {
    Map,
+   Adress
  },
  data() {
    return {
-     center:{ 
-    lat: 40.730610, 
-    lng: -73.935242
-    }
+    center:{ 
+      lat: 40.730610, 
+      lng: -73.935242
+    },
+      search: '',
+      adresses: [
+        {
+          id: 1,
+          title: 'Lille',
+        },
+        {
+          id: 2,
+          title: 'Paris',
+        },
+      ]
+   };
+ },
+ computed: {
+   filteredAdresses() {
+     return this.adresses.filter(adress => adress.title.toLowerCase().includes(this.search.toLowerCase()))
    }
  }
 }
---
+++ src/components/Adress.vue
@@ -0,0 +1,11 @@
+<template>
+    <article>
+        <h3>{{ adress.title }}</h3>
+    </article>
+</template>
+
+<script>
+    export default {
+        props: [ "adress" ]
+    };
+</script>
