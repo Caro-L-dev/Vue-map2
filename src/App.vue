2680b3916184e1044d348b4318ef8263cae3bc05
Caro-L-dev <liardcar@gmail.com>
2021-07-30 11:14:04 +0200

    :truck: Move searchbar in map component

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,31 @@
 <template>
- <div id="app">
-   <input type="text" v-model="search" >
-   <div v-for="adress in filteredAdresses" :key="adress.id">
-     <adress :adress="adress"></adress>
-   </div>
-   <Map/>
+  <div id="app">
+    <Map />
   </div>
 </template>
 
 <script>
-import Map from './components/map.vue';
-import Adress from './components/Adress.vue';
+import Map from "./components/map.vue";
 
 export default {
- name: 'App',
- components: {
-   Map,
-   Adress
- },
- data() {
-   return {
-    center:{ 
-      lat: 40.730610, 
-      lng: -73.935242
-    },
-      search: '',
-      adresses: [
-        {
-          id: 1,
-          title: 'Lille',
-        },
-        {
-          id: 2,
-          title: 'Paris',
-        },
-      ]
-   };
- },
- computed: {
-   filteredAdresses() {
-     return this.adresses.filter(adress => adress.title.toLowerCase().includes(this.search.toLowerCase()))
-   }
- }
-}
+  name: "App",
+  components: {
+    Map,
+  },
+  data() {
+    return {
+      center: {
+        lat: 40.73061,
+        lng: -73.935242,
+      },
+    };
+  },
+};
 </script>
 
 <style>
-  #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
--- src/components/map.vue
+++ src/components/map.vue
@@ -2,7 +2,11 @@
   <body>
     <div class="map--container">
       <h2>VueJs Map</h2>
-      <p>Barre de recherche</p>
+
+      <input type="text" v-model="search" placeholder="Search..." />
+      <div v-for="adress in filteredAdresses" :key="adress.id">
+        <adress :adress="adress"></adress>
+      </div>
       <l-map
         :center="center"
         :zoom="zoom"
@@ -12,11 +16,7 @@
         @update:center="centerUpdated"
       >
         <l-tile-layer :url="url"> </l-tile-layer>
-        <agence
-          v-for="marker in markers"
-          :key="marker.id"
-          :marker="marker"
-        >
+        <agence v-for="marker in markers" :key="marker.id" :marker="marker">
         </agence>
       </l-map>
     </div>
@@ -25,23 +25,46 @@
 
 <script>
 import { LMap, LTileLayer } from "vue2-leaflet";
-import Agence from './agence.vue';
+import Agence from "./agence.vue";
 import "leaflet/dist/leaflet.css";
+
+import Adress from "./Adress.vue";
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    Agence
+    Agence,
+    Adress,
   },
   data() {
     return {
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       center: [50.635342, 3.05857],
       zoom: 6,
+      search: "",
+      adresses: [
+        {
+          id: 1,
+          title: "Lille",
+        },
+        {
+          id: 2,
+          title: "Paris",
+        },
+      ],
+
       markers: [
-        { id: 1, imageUrl:"https://img.icons8.com/doodle/48/000000/pinguin.png", coordinates: [50.635342, 3.05857] }, // Lille Ylly
-        { id: 2, imageUrl: "https://img.icons8.com/doodle/48/000000/home--v1.png" , coordinates: [48.813843, 2.392828] }, // Paris Ylly
+        {
+          id: 1,
+          imageUrl: "https://img.icons8.com/doodle/48/000000/pinguin.png",
+          coordinates: [50.635342, 3.05857],
+        }, // Lille Ylly
+        {
+          id: 2,
+          imageUrl: "https://img.icons8.com/doodle/48/000000/home--v1.png",
+          coordinates: [48.813843, 2.392828],
+        }, // Paris Ylly
       ],
     };
   },
@@ -54,6 +77,11 @@ export default {
       this.center = center;
     },
   },
+   computed: {
+   filteredAdresses() {
+     return this.adresses.filter(adress => adress.title.toLowerCase().includes(this.search.toLowerCase()))
+   }
+ }
 };
 </script>
 
